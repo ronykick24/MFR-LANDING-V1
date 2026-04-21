80bdd0385f4b600901c1227932e7c3f38ee38d4e
ronykick24 <wert.pride@gmail.com>
2026-04-21 16:39:05 +0300

    Update trajectory.py

--- src/trajectory.py
+++ src/trajectory.py
@@ -11,21 +11,21 @@ def minimum_curvature(md, inc_deg, azm_deg):
     tvd = np.zeros_like(md)
 
     for i in range(1, len(md)):
-        dmd = md[i] - md[i-1]
+        dmd = md[i] - md[i - 1]
         if dmd <= 0:
             dmd = 1e-6
 
         cos_dogleg = (
-            np.sin(inc[i-1]) * np.sin(inc[i]) * np.cos(azm[i] - azm[i-1])
-            + np.cos(inc[i-1]) * np.cos(inc[i])
+            np.sin(inc[i - 1]) * np.sin(inc[i]) * np.cos(azm[i] - azm[i - 1])
+            + np.cos(inc[i - 1]) * np.cos(inc[i])
         )
         cos_dogleg = np.clip(cos_dogleg, -1.0, 1.0)
         dogleg = np.arccos(cos_dogleg)
         rf = 1.0 if dogleg < 1e-8 else 2.0 / dogleg * np.tan(dogleg / 2.0)
 
-        tvd[i] = tvd[i-1] + 0.5 * dmd * (np.cos(inc[i-1]) + np.cos(inc[i])) * rf
-        n[i] = n[i-1] + 0.5 * dmd * (np.sin(inc[i-1]) * np.cos(azm[i-1]) + np.sin(inc[i]) * np.cos(azm[i])) * rf
-        e[i] = e[i-1] + 0.5 * dmd * (np.sin(inc[i-1]) * np.sin(azm[i-1]) + np.sin(inc[i]) * np.sin(azm[i])) * rf
+        tvd[i] = tvd[i - 1] + 0.5 * dmd * (np.cos(inc[i - 1]) + np.cos(inc[i])) * rf
+        n[i] = n[i - 1] + 0.5 * dmd * (np.sin(inc[i - 1]) * np.cos(azm[i - 1]) + np.sin(inc[i]) * np.cos(azm[i])) * rf
+        e[i] = e[i - 1] + 0.5 * dmd * (np.sin(inc[i - 1]) * np.sin(azm[i - 1]) + np.sin(inc[i]) * np.sin(azm[i])) * rf
 
     hd = np.sqrt(n**2 + e**2)
-    return pd.DataFrame({'MD': md, 'INC': np.rad2deg(inc), 'AZM': np.rad2deg(azm), 'TVD': tvd, 'HD': hd})
+    return pd.DataFrame({"MD": md, "INC": np.rad2deg(inc), "AZM": np.rad2deg(azm), "TVD": tvd, "HD": hd})
